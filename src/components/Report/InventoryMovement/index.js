bc996656220cf728de765b7f312ccd48214889df
Nate <youngmini90@naver.com>
2025-04-29 16:14:16 +0900

    fix: InventoryMovement export 오류

--- src/components/Report/InventoryMovement/index.js
+++ src/components/Report/InventoryMovement/index.js
@@ -1,6 +1,6 @@
 // 통합된 컴포넌트
-export { default } from './InventoryMovementCombined';
+// export { default } from './InventoryMovementCombined';
 export { useInventoryMovement } from './InventoryMovementCombined';
 
 // 기존 코드와의 호환성을 위해 named export도 제공
-export { default as InventoryMovement } from './InventoryMovementCombined'; 
+export { default as InventoryMovement } from './InventoryMovementCombined';
